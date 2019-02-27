a94115739262a382d391230b6ff4dd3f04e52f8d
Anu Srikanth <anu.srikanth@outlook.com>
2019-02-26 21:56:02 -0500

    added first component and wired up

---
+++ src/app/events/events.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'event-component',
+    templateUrl: './events.component.html',
+    styleUrls: ['./events.component.css']
+
+})
+
+export class EventComponent {
+
+}
